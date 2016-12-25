952ebeebaf9fb76a97c5a0968db6d5d5520f6761
Jeff <jinian_2006@hotmail.com>
2016-12-25 01:53:36 -0800

    Permutations(finished)

--- Permutations/Permutations.java
+++ Permutations/Permutations.java
@@ -1,5 +1,32 @@
 package Permutations;
 
-public class Permutations {
+import java.util.*;
 
+public class Permutations {
+	public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> set = new ArrayList<>();
+        
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        
+        helper(result, set, nums);
+        return result;
+    }
+    
+    private void helper(List<List<Integer>> result, List<Integer> set, int[] nums) {
+        if (set.size() == nums.length) {
+            result.add(new ArrayList<>(set));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i]))
+                continue;
+            set.add(nums[i]);
+            helper(result, set, nums);
+            set.remove(set.size() - 1);
+        }
+    }
 }
