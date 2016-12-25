84ad915bb1fb17af0e5fe83212d44040c928d2e6
Jeff <jinian_2006@hotmail.com>
2016-12-24 18:12:07 -0800

    PalindromeNumber

---
+++ PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,19 @@
+package PalindromeNumber;
+
+public class PalindromeNumber {
+	public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        return x == reverse(x);
+    }
+    
+    private int reverse(int x) {
+        int rst = 0;
+        while(x != 0) {
+            rst = rst * 10 + x % 10;
+            x = x / 10;
+        }
+        return rst;
+    }
+}
